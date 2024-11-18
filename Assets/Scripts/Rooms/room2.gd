26f386be32a90b8e0f5f6da46ca81742b27c4142
halfwaywrong <michaelhenderson.it@gmail.com>
2024-11-18 20:10:33 +0800

    Add room_1_degraded, clean up room1 and room2

--- Assets/Scripts/Rooms/room2.gd
+++ Assets/Scripts/Rooms/room2.gd
@@ -1,7 +1,7 @@
 extends Node
 
-var room1 = load("res://Assets/Scenes/Rooms/room1.tscn")
+var room1_degraded = load("res://Assets/Scenes/Rooms/room_1_degraded.tscn")
 
 func _on_door_body_entered(body):
 	if body.name == "Player":
-		get_tree().change_scene_to_packed(room1)
+		get_tree().change_scene_to_packed(room1_degraded)
